58a01aeab5ce8e48f8cf30f3b0119864826f47e6
Gavin <7797864896@K12.LCS>
2022-11-22 15:31:43 -0500

    Create base stats

---
+++ Assets/Scripts/BaseStats.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class BaseStats : MonoBehaviour
+{
+    [SerializeField] private float _health;
+    [SerializeField] private float _attackPower;
+    [SerializeField] private float _defenseLevel;
+}
